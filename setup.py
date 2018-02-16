cdd873e46fee6443d316a1c4b83eace853faab27
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2018-02-16 14:34:51 +0100

    Import initial structure.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name='kawapai',
+    version='0.0.1',
+    description='Python library for interfacing with Kawasaki D, E and F series controllers.',
+    url='http://github.com/gavanderhoorn/kawapai',
+    author='G. Paris, G. vd. Hoorn',
+    author_email='todo@todo.todo',
+    license='TODO',
+    packages=[
+        'kawapai'
+    ],
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: TODO License',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
